fa8cf40114d522613546844b56c30f80db83bf04
储天行 <chutianxing@kuark.com>
2017-12-08 13:35:01 +0800

    bug fix

--- posix/startup.sh
+++ posix/startup.sh
@@ -14,11 +14,11 @@ echo "your wallet address is 0x$address"
 
 $BASEDIR/geth --mine \
  --rpc \
- --unlock 0x%address% \
+ --unlock 0x$address \
  --password $BASEDIR/password \
  --rpccorsdomain "*" \
  --minerthreads=1 \
- --etherbase=0x%address% \
+ --etherbase=0x$address \
  --networkid 1 \
  --bootnodes enode://f3ce7e40bfa87ebb3d740bf9e6f251aeba101f920ed50c30b4f91eaeeff65ba13d9766b7495f08b11e8aa5e2b9795309c13b0cedd7b5bcfbd1a2b47feac09b4e@121.199.5.97:30303 \
  --nodekey $BASEDIR/data/ethereum/node.key \
